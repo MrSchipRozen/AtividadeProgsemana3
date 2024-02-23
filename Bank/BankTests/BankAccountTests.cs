26d393eb09dda3594a89414300fca9593abf0861
MrSchipRozen <99350292+MrSchipRozen@users.noreply.github.com>
2024-02-23 19:40:03 -0300

    Atividade Prog

---
+++ Bank/BankTests/BankAccountTests.cs
@@ -0,0 +1,36 @@
+﻿using BankAccountNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BankTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act and assert
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.00; // Maior que o saldo
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            string expectedErrorMessage = "Specified argument was out of the range of valid values. (Parameter 'amount')"; // Mensagem de erro atualizada
+
+            // Act and assert
+            var exception = Assert.ThrowsException<ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+            StringAssert.Contains(exception.Message, expectedErrorMessage);
+        }
+    }
+}
